11fe453a099de71a244b9aa5720f36c5fb1136ca
undeadskeleton <jokerlupe1234@gmail.com>
2026-06-12 11:51:10 +0530

    Last Sync: 2026-06-12 11:51 (Mobile)

--- scripts/knob.gd
+++ scripts/knob.gd
@@ -5,17 +5,35 @@ extends Sprite2D
 @export var maxLength : int =60
 @export var deadZone : int = 10
 var pressing : bool
-var timePressed : int = 1 
+var timePressed : int = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	maxLength *= parent.scale.x
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if pressing:
-		print("Button is pressed",timePressed)
-		timePressed+=1
+		if get_global_mouse_position().distance_to(parent.global_position) <= maxLength:
+			global_position = get_global_mouse_position()
+		else:
+			var angle= parent.global_position.angle_to_point(get_global_mouse_position())
+			global_position.x = parent.global_position.x + cos(angle)*maxLength
+			global_position.y = parent.global_position.y + sin(angle)* maxLength
+		calculateVector()
+	else:
+		global_position = lerp(global_position,parent.global_position,delta*50)
+		parent.posVector = Vector2(0,0)
+	print(parent.posVector)
+
+func calculateVector()-> void:
+	if abs((global_position.x-parent.global_position.x)) >= deadZone:
+		parent.posVector.x = (global_position.x - parent.global_position.x)/maxLength
+	if abs((global_position.y - parent.global_position.y)) >= deadZone:
+		parent.posVector.y = (global_position.y - parent.global_position.y)/maxLength
+	pass
+
 
 
 func _on_button_button_down() -> void:
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,20 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var speed :int = 400
+@export var joystick : Virtual_Joystick
 var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size=get_viewport_rect().size
 
+func _physics_process(delta: float) -> void:
+	var direction = joystick.posVector
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = Vector2(0,0)
+
+	move_and_slide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
