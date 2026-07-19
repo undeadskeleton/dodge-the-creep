1a5f921257d8a254412196a4988fe17b29616bd1
undead skeleton <safepeople08@gmail.com>
2026-07-19 11:42:18 +0530

    added score system and HUD

--- Script/stage.gd
+++ Script/stage.gd
@@ -4,8 +4,8 @@ extends Node
 var score
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
-
+	get_tree().call_group("mobs","queue_free")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -14,11 +14,14 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	%ScoreTimer.stop()
 	%MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game() -> void:
 	score = 0
 	$Player.start($StartPosition.position)
 	%StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_mob_timer_timeout() -> void:
 	#Create a new instance of the mob
@@ -47,7 +50,9 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score +=1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	%MobTimer.start()
 	%ScoreTimer.start()
+	
