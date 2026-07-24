5585b2e56dd1b85a8355eb77bd64390468e12406
undead skeleton <safepeople08@gmail.com>
2026-07-24 20:18:59 +0530

    Working on input from joystick

--- Script/joystick/joystick.gd
+++ Script/joystick/joystick.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 class_name VirJoystick
-var pos : Vector2
+var posVector : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- Script/joystick/knob.gd
+++ Script/joystick/knob.gd
@@ -17,12 +17,13 @@ func _process(delta: float) -> void:
 		if parent.global_position.distance_to(get_global_mouse_position()) <= maxLength:
 			global_position= get_global_mouse_position()
 		else:
-			var angle= get_global_mouse_position().angle_to(parent.global_position)
-			global_position.x = cos(angle)* maxLength
-			global_position.y = sin(angle) * maxLength
-
+			var angle= parent.global_position.angle_to_point(get_global_mouse_position())
+			global_position.x = parent.global_position.x + cos(angle)* maxLength
+			global_position.y = parent.global_position.y + sin(angle) * maxLength
+		calculateVector()
 	else:
 		global_position = lerp(global_position,parent.global_position,delta*50)
+		parent.posVector = Vector2(0,0)
 		%Debugger.text= "False"
 
 func _on_button_button_down() -> void:
@@ -31,3 +32,11 @@ func _on_button_button_down() -> void:
 
 func _on_button_button_up() -> void:
 	pressing = false
+	
+func calculateVector()-> void:
+	if abs(global_position.x-parent.global_position.x)>= deadZone:
+		parent.posVector.x = (global_position.x-parent.global_position.x)/ maxLength
+		%Debugger2.text = "global_position.x("+str(snapped((global_position.x),0.001))+")-parent.global_position.x("+str(parent.global_position.x)+") /maxLength("+str(maxLength)+") = "+str((global_position.x-parent.global_position.x)/maxLength)
+	if abs(global_position.y-parent.global_position.y) >= deadZone:
+		parent.posVector.y = (global_position.y-parent.global_position.y) / maxLength
+		%Debugger2.text = "global_position.x("+str(snapped((global_position.x),0.001))+")-parent.global_position.x("+str(parent.global_position.x)+") /maxLength("+str(maxLength)+") = "+str((global_position.x-parent.global_position.x)/maxLength)
